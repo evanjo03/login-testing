d748e93e2be8881229c219f29283c36ebf77395d
John Evans <evanjo03@luther.edu>
2019-03-27 12:53:36 -0500

    update

--- public/js/app.js
+++ public/js/app.js
@@ -6,87 +6,42 @@ $(document).ready(function () {
     $("#login-submit-button").on("click", function (event) {
         event.preventDefault();
 
-        //get a list of all current users in the database
+        var username = $("#stored-username").val();
+        var password = $("#stored-password").val();
+        console.log("Form user:", username, password);
+        var loginObject = { username: username, password: password }
+
         $.ajax({
-            method: "GET",
-            url: "/api/users"
+            method: "POST",
+            url: "/password",
+            data: loginObject
         }).then(function (result) {
-            //store our new value variables
-            var username = $("#stored-username").val();
-            var password = $("#stored-password").val();
-            console.log("Form user:", username, password);
-
-            //create variable for our current user and booleans for login tests
-            var currentUser;
-            var usernameCorrect = false;
-            var passwordCorrect = false;
-
-            //loop through each user
-            for (var i = 0; i < result.length; i++) {
-                console.log("User:", result[i]);
-                if (result[i].username === username) {
-                    //username exists
-                    usernameCorrect = true;
-                    if (result[i].password === password) {
-                        //password is correct
-                        passwordCorrect = true;
-                        //user is assigned
-                        currentUser = result[i];
-                        //maybe use session storage or similar here?
-                        alert("Correct credentials, user logged in")
-                        console.log("Current user:", result[i]);
-                    }
-                }
-            }
-            //if the username or login were incorrect, tell the user
-            if (!(passwordCorrect && usernameCorrect)) {
-                alert("Invalid credentials");
-            }
+            alert(result);
         })
-    });
+    })
 
     //new user sign-up
     $("#new-user-submit-button").on("click", function (event) {
         event.preventDefault();
+
+        //define our new value variables
+        var username = $("#new-username").val();
+        var password = $("#new-password").val();
+
+        var newUser = {
+            username: username,
+            password: password,
+        };
+
         $.ajax({
-            method: "GET",
-            url: "/api/users"
+            method: "POST",
+            url: "/api/user",
+            data: newUser
         }).then(function (result) {
-
-            //define our new value variables
-            var username = $("#new-username").val();
-            var password = $("#new-password").val();
-            var score = 0;
-            var isUsernameUnique = true;
-
-            //check to verify there is no username in the db already under the name entered
-            for (var i = 0; i < result.length; i++) {
-                if (result[i].username === username) {
-                    isUsernameUnique = false;
-                    alert("Please enter a new username; that name is taken")
-                }
-            }
-
-            //set up variable to send to db
-            if (isUsernameUnique) {
-                var newUser = {
-                    username: username,
-                    password: password,
-                    score: score
-                };
-
-                $.ajax({
-                    method: "POST",
-                    url: "/api/user",
-                    data: newUser
-                }).then(function (result) {
-
-                    console.log("You added a new user to the db!");
-                    location.reload();
-                })
-            }
+            alert(result);
+            location.reload();
         })
-    });
+    })
 
     //loads all current users that have accounts
     function loadUsers() {
--- routing/routing.js
+++ routing/routing.js
@@ -10,22 +10,64 @@ module.exports = function (app) {
             res.json(result);
         });
     });
-    
-    app.post("/api/user", function (req, res) {
-        var password = req.body.password;
-        bcrypt.hash(password, saltRounds, function (err, hash) {
-            // Store hash in your password DB.
-            if (err) throw err;
 
-            db.User.create({
-                username: req.body.username,
-                password: hash,
-                score: 0,
-                stress: 0,
-                currentQuestionId: 0
-            }).then(function (result) {
-                res.json(result);
-            })
-        });
-    })
-};
+    app.post("/password", function (req, res) {
+        db.User.findAll({}).then(function (result) {
+            var currentUser;
+            var userExists = false;
+
+            for (var i = 0; i < result.length; i++) {
+                if (result[i].username === req.body.username) {
+                    //user is in the database, so set userExists to true
+                    currentUser = result[i];
+                    userExists = true;
+                }
+            }
+            //if user exists, check for password
+            if (userExists) {
+                bcrypt.compare(req.body.password, currentUser.password, function (error, find) {
+                    //send boolean to user with whether password is true or not
+                    if (find) {
+                        res.send("User logged in");
+                    } else {
+                        //send boolean to user with whether password is true or not
+                        res.send("Incorrect password");
+                    }
+                });
+            } else {
+                res.send("Incorrect username")
+            }
+        })
+    });
+
+    app.post("/api/user", function (req, res) {
+        var userExists = false;
+        db.User.findAll({}).then(function (result) {
+            for (var i = 0; i < result.length; i++) {
+                if (result[i].username === req.body.username) {
+                    //user is in the database, so set userExists to true
+                    userExists = true;
+                }
+            }
+            if (!userExists) {
+                var password = req.body.password;
+                bcrypt.hash(password, saltRounds, function (err, hash) {
+                    // Store hash in your password DB.
+                    if (err) throw err;
+                    db.User.create({
+                        username: req.body.username,
+                        password: hash,
+                        score: 0,
+                        stress: 0,
+                        currentQuestionId: 0
+                    }).then(function (result) {
+                        res.send(result);
+                    })
+                });
+            } else {
+                res.send("Username already exists, please try a different username!")
+            }
+
+        })
+    });
+}
